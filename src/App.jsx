3f0034863b9701975a993c39f8825f342347dc49
Osama Nazakat <usamanazakat97@gmail.com>
2023-07-08 12:01:55 +0500

    company section complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
+import CompanySection from "./components/companies/CompanySection";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <CompanySection />
     </>
   );
 }
