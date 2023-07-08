631f94197a9cc47659613d3dd1ea0dba410548c5
Osama Nazakat <usamanazakat97@gmail.com>
2023-07-08 10:22:45 +0500

    navbar complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
+import Navbar from "./components/navbar/Navbar";
+
 function App() {
   return (
     <>
-      <h1>Make payment easy, simplify your finance</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ea quae
-        exercitationem vel nam vitae culpa incidunt doloremque odio velit?
-      </p>
-      <h4>Lorem ipsum dolor sit amet.</h4>
-      <h3>Lorem ipsum dolor sit amet.</h3>
-      <a href="#" className="button-primary">
-        heelo dhlef
-      </a>
-      <a href="#" className="button-outline">
-        heelo
-      </a>
+      <Navbar />
     </>
   );
 }
