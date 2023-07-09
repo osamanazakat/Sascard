e4ff1307ae22aae2e6078140c5a0061ec9953e02
Osama Nazakat <usamanazakat97@gmail.com>
2023-07-09 10:39:26 +0500

    features section complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
-import CompanySection from "./components/companies/CompanySection";
+import CompanySection from "./components/companiesSection/CompanySection";
+import FeaturesSection from "./components/featuresSection/FeaturesSection";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <CompanySection />
+      <FeaturesSection />
     </>
   );
 }
